01b78d7a89f8e6969caae41d2bb4b71a92362264
kospetkov <fikuskos@gmail.com>
2018-02-15 00:33:59 +0200

    fixed WD_ST3_Draggable_Speech_Balloons

--- WD_ST3_Draggable_Speech_Balloons/js/jqueryFunctions.js
+++ WD_ST3_Draggable_Speech_Balloons/js/jqueryFunctions.js
@@ -1,47 +1,73 @@
 $(document).ready(function () {
-    $('.content').dblclick(function (e) {
+    $('body').on('dblclick', ".content", function (e) {
+        if (e.target.className !== 'content') {
+            return;
+        }
+        console.log(e);
         let pos = $(this).offset();
         let elem_left = pos.left;
         let elem_top = pos.top;
         let x = e.pageX - elem_left;
         let y = e.pageY - elem_top;
-        console.log("X: " + y + " Y: " + y);
-        let elem = $('<div>', {'class': 'upper_div'});
+        console.log("X: " + x + " Y: " + y);
+        let elem = $('<div>', {class: 'upper_div'});
         elem.offset({top: y, left: x});
-        let form = $('<form>');
-        let input = $('<input>', {'class': 'upper_div_textarea', 'type': 'textarea'});
-        form.append(input);
-        elem.append(form);
+        let input = $('<input>', {class: 'upper_div_textarea', type: 'textarea'});
+        let p = $('<p>', {class: 'p_for_text'});
+        p.attr({style: 'display: none;'});
+        elem.append(input);
+        elem.append(p);
         $(this).append(elem);
         input.focus();
-        $('.upper_div').draggable({
+        $(elem).draggable({
             containment: 'parent',
         });
     });
-    $('body').keydown(function (e) {
-        //if (e.keyCode !== 13 || e.keyCode !== 27) {
-        //    console.log(e.keyCode);
-        //    return;
-       // }
-        let input = $('.upper_div_textarea');
-        let value = input.val();
-        let elem = $('.upper_div');
-        if (e.keyCode === 13) {
-            input.remove();
-            let divForText = $('<div>', {'class': 'div_for_text'});
-            divForText.text(value);
-            elem.append(divForText);
+    $('.content').on('keydown', function (e) {
+        if ($('.upper_div_textarea').is( ':focus' )) {
+            if (e.keyCode !== 13 && e.keyCode !== 27) {
+                return;
+            }
+            let input = $(document.activeElement);
+            let elem = input.parent();
+            let value = input.val();
+            let y = elem.css('top');
+            let x = elem.css('left');
+            let myP = elem.find('.p_for_text');
+            if (e.keyCode === 13) {
+                if (value.length === 0) {
+                    elem.remove();
+                }
+                input.remove();
+                myP.text(value);
+                myP.css({display: 'block'});
+            }
+            else {
+                if (value.length === 0 && myP.find().length === 0 ) {
+                    elem.remove();
+                } else if (myP.find().length !== 0) {
+                    input.remove();
+                    myP.text(value);
+                    myP.css({display: 'block'});
+                    //let p = $('<p>', {class: 'p_for_text'});
+                    //p.text(value);
+                    //elem.append(p);
+                }
+            }
         }
     });
-    $('.div_for_text').dblclick(function (e) {
-        e.stopPropagation();
-        let elem = $('.div_for_text');
-        if (elem.find('.upper_div_textarea') < 1) {
-            let text = elem.text();
-            elem.text('');
-            let input = $('<input>', {'class': 'upper_div_textarea', 'type': 'textarea'});
-            elem.append(input);
-            input.focus();
-        }
+    $('.content').on('dblclick', ".upper_div", function (e) {
+        let item = $(this);
+        let myP = item.children();
+        let myPText = myP.text();
+        myP.attr({style: 'display: none;'});
+        let newInput = $('<input>', {class: 'upper_div_textarea'});
+        newInput.val(myPText);
+        item.append(newInput);
+        item.children().focus();
+        let y = item.css('top');
+        let x = item.css('left');
+        let newDiv = $('<div>', {class: 'upper_div'});
+        newDiv.css({'top': y, 'left': x});
     });
 });
