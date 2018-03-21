2594aeedbcc948a7a03d3aa6beabe6879a98bb15
kospetkov <fikuskos@gmail.com>
2018-03-21 19:35:49 +0200

    rename  to  in function chartVisual.php and added function isset

--- WD_PS4_PHP,_JSON/4.2/php/chartVisual.php
+++ WD_PS4_PHP,_JSON/4.2/php/chartVisual.php
@@ -4,13 +4,15 @@ function chartVisual()
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         header('location: index.php');
     }
-    $fileName = '../json/data.json';
-    $dataArray = json_decode(file_get_contents($fileName), true);
-    $car = $_POST['cars'];
+    $path_to_db = '../json/data.json';
+    $dataArray = json_decode(file_get_contents($path_to_db), true);
+    if (isset($_POST['cars'])) {
+        $car = $_POST['cars'];
+    }
     if (array_key_exists($car, $dataArray)) {
         $dataArray[$car]++;
     } else
     {$dataArray[$car] = 1;}
-    file_put_contents($fileName, json_encode($dataArray));
-    echo file_get_contents($fileName, true);
+    file_put_contents($path_to_db, json_encode($dataArray));
+    echo file_get_contents($path_to_db, true);
 }
