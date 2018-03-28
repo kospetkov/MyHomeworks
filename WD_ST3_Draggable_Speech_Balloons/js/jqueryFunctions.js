ad35ef9bc2d6513afbebf84e3ad0b45e773f4f12
kospetkov <fikuskos@gmail.com>
2018-03-28 21:15:51 +0300

    .

--- WD_ST3_Draggable_Speech_Balloons/js/jqueryFunctions.js
+++ WD_ST3_Draggable_Speech_Balloons/js/jqueryFunctions.js
@@ -13,27 +13,32 @@ $(document).ready(function () {
             let p = $('<p>', {class: 'p_for_text'});
             p.text(massadg);
             elem.append(p);
+            let input = $('<input>', {class: 'upper_div_textarea'});
+            input.css({display: 'none'});
+            elem.append(input);
             $('.content').append(elem);
-            $(elem).draggable({
-                containment: 'parent',
-                stop: function(event, ui) {
-                    let x = ui.position.left;
-                    let y = ui.position.top;
-                    let idElem = +elem.attr('id').replace('id_', '');
-                    $.ajax({
-                        type: 'POST',
-                        url: 'php/updateCoordInJson.php',
-                        data: {
-                            "id": idElem,
-                            "top": y,
-                            "left": x,
-                            "ident": idElem
-                        }
-                    });
-                },
-            });
         }
+
+        $('.upper_div').draggable({
+            containment: 'parent',
+            stop: function(event, ui) {
+                let x = ui.position.left;
+                let y = ui.position.top;
+                let idElem = +$(this).attr('id').replace('id_', '');
+                $.ajax({
+                    type: 'POST',
+                    url: 'php/updateCoordInJson.php',
+                    data: {
+                        "id": idElem,
+                        "top": y,
+                        "left": x,
+                        "ident": idElem
+                    }
+                });
+            },
+        });
     });
+
     $('body').on('dblclick', ".content", function (e) {
         if (e.target.className !== 'content') {
             return;
@@ -46,6 +51,8 @@ $(document).ready(function () {
         let y = e.pageY - elem_top;
         let elem = $('<div>', {class: 'upper_div'});
         elem.offset({top: y, left: x});
+        elem.attr({id: 'id_' + id});
+        id ++;
         let input = $('<input>', {class: 'upper_div_textarea', type: 'textarea'});
         let p = $('<p>', {class: 'p_for_text'});
         p.attr({style: 'display: none;'});
@@ -72,6 +79,22 @@ $(document).ready(function () {
             },
         });
     });
+
+    $('.content').on('blur', '.upper_div_textarea',  function(e){
+        let input = $(this);
+        let elem = input.parent();
+        let value = input.val();
+        let p = elem.find('.p_for_text');
+        let prevText = p.text();
+
+        if (prevText.length === 0) {
+            elem.remove();
+        } else {
+            input.css({display: 'none'});
+            p.css({display: 'block'});
+        }
+    });
+
     $('.content').on('keydown', function (e) {
         if ($('.upper_div_textarea').is( ':focus' )) {
             if (e.keyCode !== 13 && e.keyCode !== 27) {
@@ -84,45 +107,50 @@ $(document).ready(function () {
             let value = input.val();
             let p = elem.find('.p_for_text');
             if (e.keyCode === 13) {
-                if (value.length === 0) {
+
+                if (value.length === 0 && elem.attr('id')) {
+
                     let idElem = +elem.attr('id').replace('id_', '');
-                    elem.remove();
+
+                    console.log(elem);
                     $.ajax({
                         type: 'POST',
                         url: 'php/deletingInJson.php',
                         data: {"id": idElem}
                     });
+                    elem.remove();
+
                 } else {
-                    input.remove();
+                    input.css({display: 'none'});
                     p.text(value);
                     p.css({display: 'block'});
                     if (elem.attr('id')) {
-                        let idElem = +elem.attr('id').replace('id_', '');
-                        $.ajax({
-                            type: 'POST',
-                            url: 'php/addInJson.php',
-                            data: {
-                                "id": idElem,
-                                "top": y,
-                                "left": x,
-                                "msg": value,
-                                "ident": idElem
-                            }
-                        });
+                        // let idElem = +elem.attr('id').replace('id_', '');
+                        // $.ajax({
+                        //     type: 'POST',
+                        //     url: 'php/addInJson.php',
+                        //     data: {
+                        //         "id": idElem,
+                        //         "top": y,
+                        //         "left": x,
+                        //         "msg": value,
+                        //         "ident": idElem
+                        //     }
+                        // });
                     } else {
-                        elem.attr({id: 'id_' + id});
-                        id ++;
-                        $.ajax({
-                            type: 'POST',
-                            url: 'php/addInJson.php',
-                            data: {
-                                "id": id,
-                                "top": y,
-                                "left": x,
-                                "msg": value,
-                                "ident": id
-                            }
-                        });
+                        // elem.attr({id: 'id_' + id});
+                        // id ++;
+                        // $.ajax({
+                        //     type: 'POST',
+                        //     url: 'php/addInJson.php',
+                        //     data: {
+                        //         "id": id,
+                        //         "top": y,
+                        //         "left": x,
+                        //         "msg": value,
+                        //         "ident": id
+                        //     }
+                        // });
                     }
                 }
             }
@@ -130,20 +158,22 @@ $(document).ready(function () {
                 if (value.length === 0 && p.length === 0 ) {
                     elem.remove();
                 } else if (p.length > 0) {
-                    input.remove();
+                    input.css({display: 'none'});
                     p.css({display: 'block'});
                 }
             }
         }
     });
+
     $('.content').on('dblclick', ".upper_div", function (e) {
         let item = $(this);
         let p = item.children();
         let pText = p.text();
         p.attr({style: 'display: none;'});
-        let input = $('<input>', {class: 'upper_div_textarea'});
+
+        let input = item.find('.upper_div_textarea');
         input.val(pText);
-        item.append(input);
-        item.children().focus();
+        input.css({display: 'block'});
+        input.focus();
     });
 });
